f85a219409cbd124ae2a4fcdb6de8d70c28816d0
jdwil <me@jdwilliams.xyz>
2022-01-24 12:42:30 -0600

    Fixed some of the code. Removed the authorizer.

--- src/firefly_auth_middleware/application/middleware/authenticator.py
+++ src/firefly_auth_middleware/application/middleware/authenticator.py
@@ -6,32 +6,28 @@ import firefly_iaaa.domain as domain
 
 
 @ff.authenticator()
-class Authenticator(GenericOauthMiddleware):
+class Authenticator(GenericOauthMiddleware, ff.SystemBusAware):
     _request_validator: domain.OauthRequestValidators = None
+    _context_map: ff.ContextMap = None
+
+    def __init__(self):
+        self._authorization_service = self._context_map.contexts['firefly_auth_middleware'].config.get(
+            'auth_service', 'iaaa'
+        )
 
     def handle(self, message: ff.Message, *args, **kwargs):
         self.info('Authenticating')
         if self._kernel.http_request and self._kernel.secured:
             token = self._retrieve_token_from_http_request()
-            if token:
-                token = token.split(' ')[-1]
-            if token is None:
-                try:
-                    token = message.access_token
-                except:
-                    raise ff.UnauthenticatedError()
+            resp = self.request(f'{self._authorization_service}.DecodedToken', data={
+                'token': token,
+            })
+            if not isinstance(resp, dict) or 'scope' not in resp:
+                return False
 
-            self.debug('Decoding token')
-            try:
-                resp = self._get_client_user_and_token(token, self._kernel.user.id)
-                decoded = resp['decoded']
-                user = resp['user']
-                client_id = resp['client_id']
-            except:
-                raise ff.UnauthenticatedError()
-
-            self._kernel.user.token = decoded
-            self._kernel.user.scopes = decoded['scope'].split(' ')
+            self._kernel.user.token = resp
+            self._kernel.user.scopes = resp['scope'].split(' ')
+            self._kernel.user.id = resp['aud']
 
             return True
 
